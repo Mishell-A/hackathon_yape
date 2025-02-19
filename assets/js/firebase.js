12f9dce6f768c9e30057f31b56a3114961b0557e
Mishell-A <mishellarroyo2@gmail.com>
2025-02-19 12:44:19 -0500

    fix/responsive

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqtkbOsPp05UFSkvrbcFQXufRVZRlazE8",
+  authDomain: "yaprotect-g11.firebaseapp.com",
+  projectId: "yaprotect-g11",
+  storageBucket: "yaprotect-g11.firebasestorage.app",
+  messagingSenderId: "353527014941",
+  appId: "1:353527014941:web:5650ede5f6a3be776e48f4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
